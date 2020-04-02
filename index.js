0dc126338fbacdde966696d12c4d35b83323cfef
Jason Sanford <jasonsanford@gmail.com>
2020-04-02 10:20:58 -0400

    Support multiple counties

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const fetch = require('node-fetch');
 const { URLSearchParams } = require('url');
 
-const countyName = 'Iredell';
-const countyFips = '37097';
-const url = `https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=FIPS%3D${countyFips}&outFields=*&f=geojson&token=`;
+const counties = [
+  {
+    name: 'Iredell',
+    fips: 37097
+  },
+  {
+    name: 'Mecklenburg',
+    fips: 37119
+  },
+  {
+    name: 'Duplin',
+    fips: 37061
+  },
+  {
+    name: 'Gaston',
+    fips: 37071
+  }
+];
 
 async function main() {
-  const response = await fetch(url);
-  const json = await response.json();
+  const tasks = counties.map(county => {
+    return new Promise(async (resolve, reject) => {
+      const { name, fips } = county;
+      const url = `https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/ncov_cases_US/FeatureServer/0/query?where=FIPS%3D${fips}&outFields=*&f=geojson&token=`;
+      const response = await fetch(url);
+      const json = await response.json();
+      const { Confirmed: confirmed, Deaths: deaths } = json.features[0].properties;
 
-  const { Confirmed: confirmed, Deaths: deaths } = json.features[0].properties;
-  const message = `${confirmed} cases, ${deaths} deaths.`;
+      county.confirmed = confirmed;
+      county.deaths = deaths;
+
+      resolve(county);
+    });
+  });
+
+  const results = await Promise.all(tasks);
+
+  const message = results
+    .map(({ name, confirmed, deaths }) => `${name} - Confirmed: ${confirmed}, Deaths: ${deaths}`)
+    .join('\n');
 
   const outcome = await sendPushoverMessage(message);
 
@@ -18,7 +48,7 @@ async function main() {
 }
 
 async function sendPushoverMessage(message) {
-  const title = `${countyName} County Coronavirus Update`;
+  const title = 'County Coronavirus Update';
   const sound = 'classical';
   const url = 'https://api.pushover.net/1/messages.json';
   const { PUSHOVER_TOKEN: token, PUSHOVER_USER: user } = process.env;
